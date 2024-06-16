4d25b50c5e4d84a228bae3b142598cbffddd6f57
Raiymbek Baisalov <r.baisalov@gmail.com>
2024-06-16 15:37:35 +0500

    added server run

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,52 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"fmt"
+	v1 "github.com/baisalov/metricollector/internal/handler/http/v1"
+	"github.com/baisalov/metricollector/internal/server/service"
+	"github.com/baisalov/metricollector/internal/storage/memory"
+	"golang.org/x/sync/errgroup"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	storage := memory.MetricStorage{}
+
+	metricService := service.NewMetricService(storage)
+
+	h := v1.NewMetricHandler(metricService)
+
+	mux.HandleFunc(`POST /update/`, h.Update)
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	httpServer := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+		BaseContext: func(_ net.Listener) context.Context {
+			return ctx
+		},
+	}
+
+	g, ctx := errgroup.WithContext(ctx)
+	g.Go(func() error {
+		return httpServer.ListenAndServe()
+	})
+
+	g.Go(func() error {
+		<-ctx.Done()
+		return httpServer.Shutdown(context.Background())
+	})
+
+	if err := g.Wait(); err != nil {
+		fmt.Printf("exit reason: %s \n", err)
+	}
+}
