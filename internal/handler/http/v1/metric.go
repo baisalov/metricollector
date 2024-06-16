6002154385646c63832d9f766a581e4fe7d2bba3
Raiymbek Baisalov <r.baisalov@gmail.com>
2024-06-16 15:35:32 +0500

    general handler for update metric

--- internal/handler/http/v1/metric.go
+++ internal/handler/http/v1/metric.go
@@ -1,6 +1,7 @@
 package v1
 
 import (
+	"github.com/baisalov/metricollector/internal/metric"
 	"github.com/baisalov/metricollector/internal/server/service"
 	"net/http"
 	"strconv"
@@ -15,49 +16,60 @@ func NewMetricHandler(metricService *service.MetricService) *MetricHandler {
 	return &MetricHandler{service: metricService}
 }
 
-func (h *MetricHandler) GougeHandler(w http.ResponseWriter, r *http.Request) {
-	name := r.PathValue("name")
-	name = strings.TrimSpace(name)
-	if name == "" {
-		http.Error(w, "incorrect metric name", http.StatusBadRequest)
+func (h *MetricHandler) Update(w http.ResponseWriter, r *http.Request) {
+	path := strings.TrimPrefix(r.URL.Path, "/update/")
+
+	parts := strings.Split(path, "/")
+
+	metricType := metric.ParseType(parts[0])
+
+	if !metricType.IsValid() {
+		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	value, err := strconv.ParseFloat(r.PathValue("value"), 64)
-	if err != nil {
-		http.Error(w, "incorrect metric value", http.StatusBadRequest)
+	if len(parts) < 2 || parts[1] == "" {
+		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 
-	err = h.service.Gouge(r.Context(), name, value)
+	metricName := parts[1]
 
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+	if len(parts) < 3 || parts[2] == "" {
+		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	w.WriteHeader(http.StatusOK)
-}
+	metricValue := parts[2]
 
-func (h *MetricHandler) CounterHandler(w http.ResponseWriter, r *http.Request) {
-	name := r.PathValue("name")
-	name = strings.TrimSpace(name)
-	if name == "" {
-		http.Error(w, "incorrect metric name", http.StatusBadRequest)
-		return
-	}
+	switch metricType {
+	case metric.Counter:
+		value, err := strconv.Atoi(metricValue)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			http.Error(w, "incorrect counter metric value", http.StatusBadRequest)
+		}
 
-	value, err := strconv.Atoi(r.PathValue("value"))
-	if err != nil {
-		http.Error(w, "incorrect metric value", http.StatusBadRequest)
-		return
-	}
+		err = h.service.Count(r.Context(), metricName, int64(value))
 
-	err = h.service.Count(r.Context(), name, int64(value))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	default:
+		value, err := strconv.ParseFloat(metricValue, 64)
+		if err != nil {
+			http.Error(w, "incorrect gauge metric value", http.StatusBadRequest)
+			return
+		}
 
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		err = h.service.Gauge(r.Context(), metricName, value)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
 	}
 
 	w.WriteHeader(http.StatusOK)
