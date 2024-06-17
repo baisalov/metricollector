fdee50500c52367052b879b3075aa5326f1fd50d
Raiymbek Baisalov <r.baisalov@gmail.com>
2024-06-17 18:35:27 +0500

    added metric agent running

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,33 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"github.com/baisalov/metricollector/internal/agent"
+	"github.com/baisalov/metricollector/internal/agent/sender"
+	"github.com/baisalov/metricollector/internal/metric/provider"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+var (
+	pullInterval   = 2 * time.Second
+	reportInterval = 10 * time.Second
+)
+
+func main() {
+	metricAgent := agent.NewMetricAgent(&provider.MemStats{}, sender.NewHttpSender("http://localhost:8080"))
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	err := metricAgent.Run(ctx, pullInterval, reportInterval)
+
+	if err != nil {
+		log.Printf("metric agent stop: %v\n", err.Error())
+	} else {
+		log.Printf("metric agent stop")
+	}
+}
