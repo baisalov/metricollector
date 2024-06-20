27951d72335ccfa32b9ff7b45fb565160b067a86
Raiymbek Baisalov <r.baisalov@gmail.com>
2024-06-20 11:49:04 +0500

    added running flags

--- cmd/agent/flag.go
+++ cmd/agent/flag.go
@@ -12,7 +12,7 @@ var (
 )
 
 func init() {
-	flag.DurationVar(&pullInterval, "p", 2*time.Second, "interval for receiving metrics")
+	flag.DurationVar(&pullInterval, "p", 2*time.Second, "interval for pulling metrics")
 	flag.DurationVar(&reportInterval, "r", 10*time.Second, "interval for reporting")
 	flag.StringVar(&reportAddress, "a", "localhost:8080", "http address for reporting")
 
