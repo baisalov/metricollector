37b694ada497f8a08a1359dbead430ea7ea435d9
Raiymbek Baisalov <r.baisalov@gmail.com>
2024-07-31 13:21:41 +0500

    used http client with retry

--- internal/agent/sender/http.go
+++ internal/agent/sender/http.go
@@ -1,12 +1,10 @@
 package sender
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
 	"github.com/baisalov/metricollector/internal/metric"
-	"io"
+	"github.com/go-resty/resty/v2"
 	"log"
 	"net/http"
 	"strings"
@@ -15,7 +13,7 @@ import (
 
 type HTTPSender struct {
 	address string
-	client  *http.Client
+	client  *resty.Client
 }
 
 func convertMetric(m metric.Metric) metrics {
@@ -48,59 +46,39 @@ func NewHTTPSender(address string) *HTTPSender {
 		address = "http://" + address
 	}
 
+	client := resty.New()
+
+	client.
+		SetRetryCount(3).
+		SetRetryWaitTime(10 * time.Second).
+		SetRetryMaxWaitTime(30 * time.Second)
+
 	return &HTTPSender{
 		address: address,
-		client: &http.Client{
-			Timeout: 30 * time.Second,
-		},
+		client:  client,
 	}
 }
 
 func (s *HTTPSender) Send(ctx context.Context, m metric.Metric) error {
 
-	url := fmt.Sprintf("%s/update/", s.address)
+	addr := fmt.Sprintf("%s/update/", s.address)
 
 	mm := convertMetric(m)
 
 	log.Printf("sending metric: %+v\n", mm)
 
-	var buf bytes.Buffer
-
-	enc := json.NewEncoder(&buf)
-
-	err := enc.Encode(&mm)
-	if err != nil {
-		return fmt.Errorf("failed marshal metric: %w", err)
-	}
-
-	req, err := http.NewRequestWithContext(ctx, http.MethodPost, url, &buf)
-	if err != nil {
-		return fmt.Errorf("cant create request: %w", err)
-	}
-
-	req.Close = true
-
-	req.Header.Set("Content-Type", "application/json")
-
-	res, err := s.client.Do(req)
+	res, err := s.client.R().
+		SetContext(ctx).
+		SetHeader("Content-Type", "application/json").
+		SetBody(mm).
+		Post(addr)
 
 	if err != nil {
-		return fmt.Errorf("cant do request: %w", err)
+		return fmt.Errorf("failed to do request: %w", err)
 	}
 
-	defer func() {
-		if err := res.Body.Close(); err != nil {
-			log.Printf("failed to close response body: %s\n", err.Error())
-		}
-	}()
-
-	if res.StatusCode != http.StatusOK {
-		return fmt.Errorf("unexpected response status: %d", res.StatusCode)
-	}
-
-	_, err = io.ReadAll(res.Body)
-	if err != nil {
-		return fmt.Errorf("cant read response: %w", err)
+	if res.StatusCode() != http.StatusOK {
+		return fmt.Errorf("unexpected response status: %d", res.StatusCode())
 	}
 
 	return nil
