d2e702ef53acbca63b17c7dc45925e80442b86e1
Raiymbek Baisalov <r.baisalov@gmail.com>
2024-08-06 22:41:20 +0500

    added checker

---
+++ internal/checker/checker.go
@@ -0,0 +1,62 @@
+package checker
+
+import (
+	"context"
+	"slices"
+	"strings"
+	"sync"
+)
+
+type Checker struct {
+	mx     sync.Mutex
+	checks []CheckFunc
+}
+
+func NewChecker() *Checker {
+	return &Checker{}
+}
+
+type CheckFunc func(ctx context.Context) error
+
+func (c *Checker) Register(fn CheckFunc) {
+	c.mx.Lock()
+	defer c.mx.Unlock()
+
+	c.checks = append(c.checks, fn)
+}
+
+func (c *Checker) Check(ctx context.Context) error {
+	c.mx.Lock()
+
+	checks := slices.Clone(c.checks)
+
+	c.mx.Unlock()
+
+	var errs checkErrors
+
+	for _, check := range checks {
+		if err := check(ctx); err != nil {
+			errs = append(errs, err)
+		}
+	}
+
+	return errs
+}
+
+type checkErrors []error
+
+func (e checkErrors) Error() string {
+	var s []string
+
+	for _, err := range e {
+		s = append(s, err.Error())
+	}
+
+	return strings.Join(s, ", ")
+}
+
+func Wrap(fn func() error) CheckFunc {
+	return func(_ context.Context) error {
+		return fn()
+	}
+}
