7083ffeec2a6f6bee57ac51a3abcf4491ee06529
Raiymbek Baisalov <r.baisalov@gmail.com>
2024-11-12 11:39:02 +0500

    refactored and wtf

--- internal/server/handler/http/middleware/hash.go
+++ internal/server/handler/http/middleware/hash.go
@@ -5,53 +5,69 @@ import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/hex"
+	"fmt"
 	"io"
 	"log/slog"
 	"net/http"
 )
 
-func HashCheck(hashKey string) func(next http.Handler) http.Handler {
+type hashStatusReWriter struct {
+	http.ResponseWriter
+}
+
+func (w hashStatusReWriter) WriteHeader(statusCode int) {
+	if statusCode < 500 {
+		w.ResponseWriter.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	w.ResponseWriter.WriteHeader(statusCode)
+}
+
+func HashCheck(key string) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-			header := r.Header.Get("HashSHA256")
+			if r.Header.Get("HashSHA256") != "" && r.Method == http.MethodPost {
 
-			if header != "" {
-
-				slog.Debug("hash header", "sum", header)
-
-				sum, err := hex.DecodeString(header)
+				isCorrect, err := checkHash(r, key)
 				if err != nil {
-					slog.Error("failed to decode hex string", "error", err)
+					slog.Error(err.Error())
 					w.WriteHeader(http.StatusInternalServerError)
 					return
 				}
 
-				var buf bytes.Buffer
-
-				tr := io.TeeReader(r.Body, &buf)
-
-				data, err := io.ReadAll(tr)
-				if err != nil {
-					slog.Error("failed to read request body", "error", err)
-					w.WriteHeader(http.StatusInternalServerError)
-					return
-				}
-
-				r.Body = io.NopCloser(&buf)
-
-				h := hmac.New(sha256.New, []byte(hashKey))
-				h.Write(data)
-				sign := h.Sum(nil)
-
-				if !bytes.EqualFold(sign, sum[:]) {
-					slog.Error("invalid body sign")
-					w.WriteHeader(http.StatusBadRequest)
-					return
+				if !isCorrect {
+					slog.Warn("invalid body sign")
+					w = hashStatusReWriter{ResponseWriter: w}
 				}
 			}
 
 			next.ServeHTTP(w, r)
 		})
 	}
+}
+
+func checkHash(r *http.Request, key string) (bool, error) {
+	sum, err := hex.DecodeString(r.Header.Get("HashSHA256"))
+	if err != nil {
+		return false, fmt.Errorf("failed to decode hex string: %w", err)
+	}
+
+	var buf bytes.Buffer
+
+	tr := io.TeeReader(r.Body, &buf)
+
+	data, err := io.ReadAll(tr)
+	if err != nil {
+		return false, fmt.Errorf("failed to read request body: %w", err)
+	}
+
+	r.Body = io.NopCloser(&buf)
+
+	h := hmac.New(sha256.New, []byte(key))
+	h.Write(data)
+	sign := h.Sum(nil)
+
+	return bytes.EqualFold(sign, sum[:]), nil
 }
